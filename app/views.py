e2d0e6b4d7c34a2cf9b15f386ef82660dccc96cf
swayam02072001 <swayam02072001@gmail.com>
2023-11-22 15:14:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def swayam(request):
+    return render(request, 'swayam.html')
+
+def akankshya(request):
+    return render(request, 'akankshya.html')
+
+def akankshya2(request):
+    return render(request, 'akankshya2.html')
